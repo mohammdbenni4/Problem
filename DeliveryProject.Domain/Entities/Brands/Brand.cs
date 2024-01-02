2841cf1e9fbd188daac3a2886368bb5b498375b3
mohammd benni <mohammdbenni4@gmail.com>
2024-01-02 13:00:50 +0300

    init

---
+++ DeliveryProject.Domain/Entities/Brands/Brand.cs
@@ -0,0 +1,21 @@
+﻿//using DeliveryProject.Domain.Entities.Base;
+
+//namespace DeliveryProject.Domain.Entities.Brands;
+
+//public class Brand : Entity
+//{
+//	private Brand()
+//	{
+//	}
+
+//	public string Name { get; private set; }
+
+//	public string Governorate { get; private set; }
+
+//	public string BranchName { get; private set; }
+
+//	public int FounderPhone { get; private set; }
+
+//	//public 
+//}
+
---
+++ DeliveryProject.Persistence/Migrations/20240102072453_TestTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DeliveryProject.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestModels",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestModels", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestModels");
+        }
+    }
+}
